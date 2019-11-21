17b40746d5069c25267166ab37f382761ca7f0b3
Chiara Mizzi <chiara.mizzi@gmail.com>
2019-11-21 17:38:55 +0100

    edit query_mariadb, preprocessing wip

--- script/pixl-1query_mariadb.py
+++ script/pixl-1query_mariadb.py
@@ -15,43 +15,32 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--cfg', help='config file', required=True)
 args = parser.parse_args()
 
+# load toi
+toi_dict = {'station'    :[140043, 93843],
+            'square'     :[140058, 93850],
+            'stnuova'    :[140053, 93842],
+            'center'     :[140057, 93849],
+            'rialto'     :[140052, 93846],
+            'port'       :[140037, 93852],
+            'academy'    :[140049, 93852],
+            'university' :[140046, 93850]
+            }
+
 # load config
 with open(args.cfg) as f:
   config = json.load(f)
 
-#query = 'SHOW DATABASES;'
-#query = 'SHOW TABLES;'
-#query = 'SELECT * FROM cuore;'
-#query = 'DESCRIBE test_year;'
-#query = """SELECT time, P FROM test_year
-#WHERE
-#  tileX = 140057
-#  AND
-#  tileY = 93850
-#  AND
-#  year = 17 
-#  AND
-#  month = 9
-#  AND
-#  day = 3
-#;
-#""" ##15 min
+query_string = '\n'.join([
+  'SELECT tileX, tileY, month, day, time, holiday, P FROM test_year',
+  'WHERE(',
+  *['(tileX = {} AND tileY = {}) {}'.format(v[0], v[1], 'OR' if i != len(toi_dict) - 1 else '') for i,v in enumerate(toi_dict.values())],
+  ')',
+  'AND',
+  'year = 17',
+  ';'
+])
 
-query = """SELECT tileX, tileY, month, day,P FROM test_year   
-WHERE ((tileX = 140058 AND tileY = 93850) #square
-  OR (tileX = 140043 AND tileY = 93843)   #station
-  OR (tileX = 140053 AND tileY = 93842)   #stnuova
-  OR (tileX = 140057 AND tileY = 93849)   #center
-  OR (tileX = 140052 AND tileY = 93846)   #rialto
-  OR (tileX = 140037 AND tileY = 93852)   #port
-  OR (tileX = 140048 AND tileY = 93854))  #academy
-  AND
-  year = 17 
-  AND
-  time = 900
-;
-""" ## 33 min
-
+query = query_string
 
 try:
   db = mysql.connector.connect(
@@ -67,16 +56,10 @@ try:
   #result = cursor.fetchall()
 
   df = pd.read_sql(query, con=db)
-  df = df.sort_values(by =['tileX','month','day'] , ascending=True)
-  df.plot('P',color='red',style='.-')
-  df.to_csv(args.cfg + '.csv', index=None, sep=';')
+  df = df.sort_values(by =['tileX','month','day', 'time'] , ascending=True)
+  df.to_csv('toi_P.csv', index=None, sep=';')
   tquery = datetime.now() - tquery
   print('[Query-MariaDB] Query for "{}" took {}'.format(query, tquery))
-  plt.show()
-
-  #for x in result:
-  #  print(x)
-
 
 except Exception as e:
   print('[Query-MariaDB] Error : {}'.format(e))
--- script/pixl-2preprocessing.py
+++ script/pixl-2preprocessing.py
@@ -1,35 +1,57 @@
-import pandas as pd 
-import numpy as np 
-import seaborn as sns 
+import pandas as pd
+import numpy as np
+import seaborn as sns
 import sys
 
 sns.set()
 
-if len(sys.argv) < 3:
-	print("Usage :", sys.argv[0], "path/to/csv/input", "path/to/csv/output")
+
+if len(sys.argv) < 2:
+	print("Usage :", sys.argv[0], "path/to/csv/input")
 	exit(1)
 
 input_file = sys.argv[1]
-output_file = sys.argv[2]
 
 df_input  = pd.read_csv(input_file, sep=';')
-df_output = pd.read_csv(output_file, sep=';')
+time_group = df_input.groupby(['time'])
+time_list = list(time_group.groups.keys())
+dict_dftime = {}
+for j in time_list:
+	df_time = time_group.get_group(j)
+	group_df  =  df_time.groupby(['tileX','tileY'])
+	tile_list = list(group_df.groups.keys())
+	df_in =pd.DataFrame(columns=['month','day'])
 
-group_input  =  df_input.groupby(['tileX','tileY'])
-group_output = df_output.groupby(['tileX','tileY'])
-tile_list_in = list(group_input.groups.keys())
-tile_list_out = list(group_output.groups.keys())
+	cnt = 0
+	for i in tile_list:
+		df_tile = group_df.get_group(i)
+		df_tile = df_tile[df_tile.holiday == 0]
+		df_tile = df_tile.drop(['tileX', 'tileY', 'time','holiday'], axis=1)
+		tile_name = str(i[0])+'_'+str(i[1])
+		df_tile = df_tile.rename(columns={'P':tile_name}, inplace=False)
+		df_in = pd.merge(df_in,df_tile, how='outer', on=['month','day'])
 
-df_in =pd.DataFrame(columns=['month','day'])
-df_out =pd.DataFrame(columns=['month','day'])
+	dict_dftime[j] = df_in
 
-for i in tile_list_in:
-	df_tile = group_input.get_group(i)
-	df_tile = df_tile.drop(['tileX', 'tileY'], axis=1)
-	tile_name = str(i[0])+'_'+str(i[1])
-	df_tile = df_tile.rename(columns={'P':tile_name}, inplace=False)
-	df_in = pd.merge(df_in,df_tile, how='outer', on=['month','day'])
+name_tile = []
+for x in tile_list:
+	tile_name = str(x[0])+'_'+str(x[1])
+	name_tile.append(tile_name)
 
+for i in np.arange(0,len(time_list)-1):
+		for j in name_tile:
+			out_tile = j
+			in_tile = name_tile.copy()
+			in_tile.remove(j)
+			print('-------------')
+			print(out_tile)
+			print(in_tile)
+			print(dict_dftime[time_list[i]])
+			print(dict_dftime[time_list[i+1]])
+			print('-------------')
+			exit(1)
+
+'''
 for i in tile_list_out:
 	df_tile = group_output.get_group(i)
 	df_tile = df_tile.drop(['tileX', 'tileY'], axis=1)
@@ -39,3 +61,4 @@ for i in tile_list_out:
 
 df_in.to_csv('data/input_matrix.csv', sep='\t', index=False)
 df_out.to_csv('data/output_matrix.csv', sep='\t', index=False)
+'''

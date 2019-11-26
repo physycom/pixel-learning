69a9ec20fb70c53e76f43fbf12804c6b1f52d625
Chiara Mizzi <chiara.mizzi@gmail.com>
2019-11-26 17:40:50 +0100

    add features selection

--- script/pixl-3network_learning.py
+++ script/pixl-3network_learning.py
@@ -10,6 +10,8 @@ from os.path import isfile, join
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
 from datetime import datetime
 
 # parse command line
@@ -28,7 +30,7 @@ file_list = [f for f in listdir(dir_path) if (isfile(join(dir_path, f)) and f.en
 weight_matrix = pd.DataFrame(columns=['Hour', 'Tile_out', 'Tile_in', 'Intercept', 'Slope'])
 with open('metadata.csv', 'w') as out:
   for i in file_list:
-    print('[PL-3-netlearn] Training model : {}'.format(i), flush=True)
+    #print('[PL-3-netlearn] Training model : {}'.format(i), flush=True)
 
     tnow = datetime.now()
 
@@ -47,16 +49,21 @@ with open('metadata.csv', 'w') as out:
     y = df[y_name]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
+    # Feature Extraction with Univariate Statistical Tests (Chi-squared for classification)
+    test = SelectKBest(f_regression, k=10)
+    fit = test.fit(X_train, y_train)
+    X_train_new = fit.transform(X_train)
+    X_test_new = fit.transform(X_test)
+    X_name = df.columns[fit.get_support(indices=True)].to_list()
     ####################### LINEAR REGRESSION ############################
     model = LinearRegression()
-    model.fit(X_train,y_train)
-    r_sq = model.score(X_test,y_test)
+    model.fit(X_train_new,y_train)
+    r_sq = model.score(X_test_new,y_test)
     out.write('{},{},{:.3f}\n'.format(time, y_name, r_sq))
     weight_matrix = weight_matrix.append(pd.DataFrame([[time, y_name, X_name, model.intercept_, model.coef_]],
                     columns=['Hour', 'Tile_out', 'Tile_in', 'Intercept', 'Slope']))
 
-    print('[PL-3-netlearn] Model perf {} trained in {}'.format(r_sq, datetime.now() - tnow), flush=True)
+    #print('[PL-3-netlearn] Model perf {} trained in {}'.format(r_sq, datetime.now() - tnow), flush=True)
 
 out.close
 
